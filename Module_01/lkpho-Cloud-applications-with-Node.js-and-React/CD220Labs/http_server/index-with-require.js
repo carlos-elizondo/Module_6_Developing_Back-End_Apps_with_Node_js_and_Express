9a20697b0fd344f71d02cf91831a28181e69e79d
carlos-elizondo <122058238+carlos-elizondo@users.noreply.github.com>
2026-04-22 13:10:19 -0600

    - Initial commit for Hands-on Lab - Promise Callback

--- Module_01/lkpho-Cloud-applications-with-Node.js-and-React/CD220Labs/http_server/index-with-require.js
+++ Module_01/lkpho-Cloud-applications-with-Node.js-and-React/CD220Labs/http_server/index-with-require.js
@@ -1,14 +1,45 @@
- // Import the HTTP module
-const http = require('http');
+// Import the HTTP module
+const http = require("http");
 
 // Import the 'today' module
-const today = require('./today');
+const today = require("./today");
 
 // Define the request listener function
 const requestListener = function (req, res) {
     res.writeHead(200); // Set the status code to 200 (OK)
     // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
+    const date = new Date();
+    const year = date.getFullYear();
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    const month = date.getMonth();
+    const day = date.getDate();
+    const time = date.getHours();
+    let message = "";
+    if (time > 5 && time < 12) {
+        message = `Hello, good morning! The date today is ${month} ${day} ${year}`;
+    } else if (time > 12 && time < 18) {
+        message = `Hello, good afternoon! The date today is ${months[month]} ${day} ${year}`;
+    } else if (time > 18 && time < 21) {
+        message = `Hello, good evening! The date today is ${months[month]} ${day} ${year}`;
+    } else if (time > 21 && time < 24) {
+        message = `Hello, good night! The date today is ${month} ${day} ${year}`;
+    } else {
+        message = `Better go to bed! The date today is ${month} ${day} ${year}`;
+    }
+    res.end(message);
 };
 
 // Define the port number
@@ -19,4 +50,4 @@ const server = http.createServer(requestListener);
 
 // Start the server and listen on the specified port
 server.listen(port);
-console.log('Server listening on port: ' + port);
+console.log("Server listening on port: " + port);
